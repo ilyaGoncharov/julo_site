6cbd460c9c36ce66db0130918d0330d789259fbb
Ilya Goncharov <karnebero@gmail.com>
2016-12-23 20:03:45 +0300

    menu icon

--- site/js/script.js
+++ site/js/script.js
@@ -1,8 +1,29 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$('#crossburger').click(function(){
 		$(this).toggleClass('open');
 	});
-});
+});*/
+
+
+(function() {
+
+  "use strict";
+
+  var toggles = document.querySelectorAll(".c-hamburger");
+
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+    });
+  }
+
+})();
 
 var status = 0;
 var texter = document.getElementById('texter');
@@ -28,6 +49,7 @@ function show(menu) {
 	    status = 0;
 	} 
 };
+
 
 
 
