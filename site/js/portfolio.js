f939967b92c59c27600d55b59eaeee64e56d47ff
Ilya Goncharov <karnebero@gmail.com>
2017-02-20 23:59:01 +0300

    redesign 1.0

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,1 +1,23 @@
-/*		document.body.style.overflowY = "hidden";*/
+$(document).scroll(function () {
+	var carusel = 0;
+    var y = $(this).scrollTop();
+    if (y > 10 && status == 0) {
+        $('.icon__bar').fadeIn();
+        $('.arrow').fadeOut();
+        /*carusel = 1;*/
+    } else {
+        $('.icon__bar').fadeOut();
+        $('.arrow').fadeIn();
+        /*carusel = 0;*/
+    }
+});
+
+$(document).ready(function(){
+     $('body,html').animate({scrollTop: 156}, 1600); 
+});
+
+$('.carousel').carousel({
+  interval: 10000,
+  pause: 'hover',
+  wrap: true
+});
--- site/js/script.js
+++ site/js/script.js
@@ -1,54 +1,59 @@
+var status = 0;
+var word = document.getElementById('form');
+var blur = document.getElementById('inner__wrapper');
+/*var greybody = document.getElementsByTagName("BODY");*/
+function show(form) {
+	if (status == 0) {
+		form.style.zIndex= "5";
+		form.style.opacity = "1";
+		blur.style.filter = "blur(6px)";
+		/*greybody.style.filter = "grayscale(100%)";*/
+	    status = 1;
+	} else {
+		form.style.zIndex= "-5";	
+	    form.style.opacity = "0";
+	    blur.style.filter = "blur(0px)";
+	    $(document).ready(function(){
+		     $('body,html').animate({scrollTop: 156}, 1600); 
+		});
+	    status = 0;
+	} 
+};
+
+/*var menu = document.getElementById("myMenu");
+var wrapper = document.getElementById("wrapper");	
+function openMenu() {
+   menu.style.top = "0px";
+   // menu.style.transition = "0.8s";
+   fadebar.style.wrapper = "0";
+   fadebar.style.filter  = 'alpha(opacity=00)'; // IE fallback
+}
+function closeMenu() {
+   menu.style.top = "-900px";
+   menu.style.transition = "0.8s";
+}
+*/
+
 /*$(document).ready(function(){
 	$('#crossburger').click(function(){
 		$(this).toggleClass('open');
 	});
 });*/
 
-
-(function() {
-
+/*(function() {
   "use strict";
-
   var toggles = document.querySelectorAll(".c-hamburger");
-
   for (var i = toggles.length - 1; i >= 0; i--) {
     var toggle = toggles[i];
     toggleHandler(toggle);
   };
-
   function toggleHandler(toggle) {
     toggle.addEventListener( "click", function(e) {
       e.preventDefault();
       (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
     });
   }
-
-})();
-
-var status = 0;
-var texter = document.getElementById('texter');
-var texterInner = document.getElementById('texter__inner');
-function show(menu) {
-	if (status == 0) {
-		menu.style.opacity= "1";
-		menu.style.zIndex= "3";
-		menu.style.transition = "0.2s";
-
-		logo1.style.display= "block";
-		logo0.style.display= "none";
-
-	    status = 1;
-	} else {
-		menu.style.opacity= ".0";
-		menu.style.zIndex= "0";
-	    menu.style.transition = "0.3s";
-
-	    logo1.style.display= "none";
-		logo0.style.display= "block";
-
-	    status = 0;
-	} 
-};
+})();*/
 
 
 
